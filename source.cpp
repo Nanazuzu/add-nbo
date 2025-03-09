09f394ebf5f40330cfc3579af324978ebd5c9419
nanazuzu <emps1308@gmail.com>
2025-03-09 20:12:29 +0900

    Function Divided, source.cpp(in other name, main)

--- source.cpp
+++ source.cpp
@@ -12,45 +12,12 @@ int main(int argc, char* argv[])
 	for(int i = 1; i < argc; i++)
 	{
 		char* str = argv[i];
-		FILE* bin_file = fopen(str, "rb");
-
-		if(bin_file == NULL)
-		{
-			printf("file is not exist\n");
-			return 1;
-		}
-
-		uint32_t* res = (uint32_t *)malloc(sizeof(uint32_t));
-		if(res == NULL)
-		{
-			printf("malloc fail\n");
-			fclose(bin_file);
-			return 1;
-		}
-
-		size_t read = fread(res, sizeof(uint32_t), 1, bin_file);
-		if(read != 1)
-		{
-			printf("read failed\n");
-			free(res);
-			fclose(bin_file);
-			return 1;
-		}
-
-		uint32_t n1, n2, n3, n4, n;
-		n1 = (*res & 0xff000000) >> 24;
-		n2 = (*res & 0x00ff0000) >> 8;
-		n3 = (*res & 0x0000ff00) << 8;
-		n4 = (*res & 0x000000ff) << 24;
-		n = n1 | n2 | n3 | n4;
-
-		printf("%d(0x%08x) ", n, n);
+		uint32_t input_num = file_handler(str);
+		uint32_t ordered_num = byte_order(input_num);
+		printf("%d(0x%08x) ", ordered_num, ordered_num);
 		if(i - 1 != argc - 2)
 			printf("+ ");
-		sum += n;
-
-		free(res);
-		fclose(bin_file);
+		sum += ordered_num;
 	}
 	printf("= %d(0x%08x)\n", sum, sum);
 	return 0;
